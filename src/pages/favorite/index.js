6fbecda979d9e20064deb87d47cec1bee5c9f424
samuelg <sam2018gs@gmail.com>
2025-03-03 15:53:25 -0300

    Removendo favoritos

--- src/pages/favorite/index.js
+++ src/pages/favorite/index.js
@@ -1,3 +1,62 @@
+import { useEffect, useState } from "react";
+import Container from "../../components/Container";
+import { ButtonStyled, Card, Content, LinkStyled } from "./styles";
+import { rawgApi, tokenRawg } from "../../services/apis/RAWG";
+import { FaRegTrashCan } from "react-icons/fa6";
+import { toast } from "react-toastify";
+
 export default function Favorite() {
-  return <div></div>;
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    loadLocal()
+  }, [])
+
+  async function loadLocal() {
+    const storedIds = JSON.parse(localStorage.getItem('@game_id')) || []
+    if (storedIds.length > 0) {
+      try {
+        const gamesData = await Promise.all(
+          storedIds.map(async (id) => {
+            const response = await rawgApi.get(`/games/${id}`, {
+              params: { key: tokenRawg },
+            });
+            return response.data
+          })
+        )
+        setGames(gamesData)
+      } catch (error) {
+        console.log("Erro ao carregar jogos favoritos:", error.message)
+      }
+    }
+  }
+
+  function handleDelete(id) {
+    const updatedGames = games.filter(game => id !== game.id)
+    const updatedIds = updatedGames.map((game) => game.id);
+    localStorage.setItem('@game_id', JSON.stringify(updatedIds))
+    if (updatedGames.length === 0) {
+      localStorage.removeItem("@game_id");
+    }
+    setGames(updatedGames)
+    toast.success('Jogo excluido dos favoritos.')
+  }
+
+  return (
+    <Container>
+      <Content>
+        {games.length === 0 ? (<h3 style={{ color: '#FFFFFF' }}>Não possuem jogos salvos</h3>) : (<h1 style={{ color: '#FFFFFF', marginBottom: 25 }}>Lista de favoritos</h1>)}
+        {games.map((game) => (
+          <Card key={game?.id}>
+            <LinkStyled to={`/detail/${game?.id}`}>
+              {game.name}
+            </LinkStyled>
+            <ButtonStyled onClick={() => handleDelete(game?.id)}>
+              <FaRegTrashCan size={30} />
+            </ButtonStyled>
+          </Card>
+        ))}
+      </Content>
+    </Container>
+  );
 }
--- src/pages/favorite/styles.js
+++ src/pages/favorite/styles.js
@@ -1,3 +1,62 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-export {};
+const Content = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 50px;
+    flex-direction: column;
+`;
+
+const Card = styled.div`
+    background-color:#0E5C88;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #FF455F;
+    padding: 1.5em;
+    width: 100%;
+    margin-bottom: 15px;
+    border-radius: 8px;
+    box-shadow: 4px 4px 10px 4px rgba(0, 0, 0, 0.57);
+`;
+
+const LinkStyled = styled(Link)`
+    text-decoration: none;
+    cursor: pointer;
+    font-size: 22px;
+    color: #FFFFFF;
+    transition: all  1s;
+    &:hover{
+        color:rgb(0, 0, 0);
+        text-decoration: underline;
+        text-decoration-color: #FFFFFF;
+        text-underline-offset: 5px;
+    }   
+`;
+
+const ButtonStyled = styled.button`
+    cursor: pointer;
+    padding: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    color: #FF455F;
+    transition: ease-in-out 0.6s;
+    &:hover{
+        box-shadow: 4px 4px 10px 4px rgba(0, 0, 0, 0.57);
+        color:rgb(255, 255, 255);
+    }
+    &:active{
+        transform: scale(1.1)
+    }
+`;
+
+export {
+    Content,
+    Card,
+    LinkStyled,
+    ButtonStyled
+};
